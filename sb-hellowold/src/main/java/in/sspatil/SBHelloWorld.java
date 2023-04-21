835cbd2f7f9c9f281f32617738d04ade2c53a828
suraj patil <3surajpatil@gmail.com>
2023-04-21 18:33:30 +0530

    added sb-hellowold app
    added sb-hellowold app

---
+++ sb-hellowold/src/main/java/in/sspatil/SBHelloWorld.java
@@ -0,0 +1,13 @@
+package in.sspatil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBHelloWorld
+{
+public static void main(String[] args) 
+{
+SpringApplication.run(SBHelloWorld.class, args);
+}
+}
